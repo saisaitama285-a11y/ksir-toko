bc9f9cb1cc8721fe727e771089b3d34a17187c82
saisaitama285-a11y <saisaitama285@gmail.com>
2025-12-30 23:10:42 +0700

    Remove redundant Firebase config properties

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,26 +1,13 @@
 // Ganti dengan data dari Firebase Console > Project Settings > Web Apps
 const firebaseConfig = {
-
-
-  apiKey: "AIzaSyCqlgi-B8nUBRuL8bHh10rBuAdIQ9EBk9U",
-
-
-  authDomain: "kasir-toko-2eb38.firebaseapp.com",
-
-
-  projectId: "kasir-toko-2eb38",
-
-
-  storageBucket: "kasir-toko-2eb38.firebasestorage.app",
-
-
-  messagingSenderId: "384536807400",
-
-
-  appId: "1:384536807400:web:6e05e4677423449306865d",
-
-
-  };
+  apiKey: "AIzaSyCqlgi-B8nUBRuL8bHh10rBuAdIQ9EBk9U",
+  authDomain: "kasir-toko-2eb38.firebaseapp.com",
+  projectId: "kasir-toko-2eb38",
+  storageBucket: "kasir-toko-2eb38.firebasestorage.app",
+  messagingSenderId: "384536807400",
+  appId: "1:384536807400:web:6e05e4677423449306865d",
+  measurementId: "G-R9NMJNQVXG"
+};
 
 // Inisialisasi Firebase
 if (!firebase.apps.length) {
